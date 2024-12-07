d921d4fdb2d5ba277424caf3cffcdf177c6a811e
Hamza <hamzabenkorichetu@gmail.com>
2024-12-07 17:26:46 +0000

    valid url

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -107,7 +107,22 @@ class ProductController extends Controller
         }
         if(!$request->query('showAll')){
 
-            $builder->whereNotNull('img')->where('img', '!=', '');
+            $builder->whereNotNull('img')
+                ->where('img', '!=', '')
+                ->where(function ($q) {
+                    $q->where(function ($q) {
+                        $q->where('img', 'LIKE', 'http%')
+                            ->where('img', 'LIKE', '%.jpg')
+                            ->orWhere('img', 'LIKE', '%.jpeg')
+                            ->orWhere('img', 'LIKE', '%.png')
+                            ->orWhere('img', 'LIKE', '%.gif')
+                            ->orWhere('img', 'LIKE', '%.webp');
+                    })->orWhere(function ($q) {
+                        $q->where('img', 'LIKE', 'data:image/%')
+                            ->where('img', 'LIKE', '%;base64,%');
+                    });
+                });
+
         }else{
 
 
