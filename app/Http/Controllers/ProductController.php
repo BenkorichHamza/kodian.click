afeed0bd2f568c9dd76bb56417c87ec922bd5c86
Hamza <hamzabenkorichetu@gmail.com>
2024-12-10 19:29:41 +0000

    correct

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -85,7 +85,7 @@ class ProductController extends Controller
             ->orWhereHas('brand', fn($q) => $q->where("description","LIKE","%".$query1."%"))
             ->orWhereHas('brand', fn($q) => $q->where("descriptionAr","LIKE","%".$query1."%"));
         });
-        $builder->orWhere(function ($q) use ($words) {
+        $builder->where(function ($q) use ($words) {
             foreach ($words as $word) {
                     $q->where(function ($q1) use ($word) {
                         $q1->where("name","LIKE","%".$word."%")
