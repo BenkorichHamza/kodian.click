188673c59b48579ed2ee3c7c34a26feec5a903ac
Hamza <hamzabenkorichetu@gmail.com>
2024-12-27 09:57:51 +0000

    order categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -17,7 +17,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return CategoryResource::collection(Category::all());
+        return CategoryResource::collection(Category::orderBy('order')->get());
     }
 
     public function products(Category $category)
