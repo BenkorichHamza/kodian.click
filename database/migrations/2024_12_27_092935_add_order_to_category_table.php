b23df8144d7aa0491921d922af225110d8acc9c8
Hamza <hamzabenkorichetu@gmail.com>
2024-12-27 10:18:36 +0000

    corrcet

--- database/migrations/2024_12_27_092935_add_order_to_category_table.php
+++ database/migrations/2024_12_27_092935_add_order_to_category_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->integer('order')->default(0);
+            $table->integer('order');
         });
     }
 
