1e9dd03e1f1d614fe26e5d74f199a19769ff6007
Hamza <hamzabenkorichetu@gmail.com>
2024-12-27 10:01:31 +0000

    correct

--- database/migrations/2024_12_27_092935_add_order_to_category_table.php
+++ database/migrations/2024_12_27_092935_add_order_to_category_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('catergories', function (Blueprint $table) {
+        Schema::table('categories', function (Blueprint $table) {
             $table->integer('order')->default(0);
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('catergories', function (Blueprint $table) {
+        Schema::table('categories', function (Blueprint $table) {
             $table->dropColumn('order');
         });
     }
