afa2b06ee8a26d16021b12916d5b6c25238cf636
Hamza <hamzabenkorichetu@gmail.com>
2025-01-11 15:12:34 +0000

    correct unique img name

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -309,7 +309,7 @@ class ProductController extends Controller
         if ($request->hasFile('image')) {
             $img = $request->file('image');
 
-            $filename = time() . '.' . $img->extension();
+            $filename = Str::uuid() . '.' . $img->extension();
             $image = Image::read($img);
             $image
             ->resize(1000,1000, function($constraint){
