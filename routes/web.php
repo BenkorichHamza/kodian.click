57c36571f1ec33fc38d2c9e272c817a7bb99b350
Hamza <hamzabenkorichetu@gmail.com>
2025-02-07 18:28:32 +0100

    home page

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('Home');
 });
 
 Route::get('/privacy-policy', function () {
