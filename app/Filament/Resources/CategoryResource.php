cfa5a360bc27b5ece6c2c7fbe3864ba318aad75c
Hamza <hamzabenkorichetu@gmail.com>
2025-02-21 14:46:18 +0100

    corect

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -55,7 +55,7 @@ class CategoryResource extends Resource
                 "img" => Tables\Columns\ImageColumn::make('img'),
               "name" => Tables\Columns\TextColumn::make('name')->searchable(),
             //   "order" => Tables\Columns\TextColumn::make('order')->sortable(),
-              "order" => Tables\Columns\TextInputColumn::make('order')->sortable(),
+              "order" => Tables\Columns\TextInputColumn::make('order')->sortable()->type('number'),
               "namAr" => Tables\Columns\TextColumn::make('nameAr'),
               "parentName" => Tables\Columns\TextColumn::make('parent.name')->searchable(),
             ])
